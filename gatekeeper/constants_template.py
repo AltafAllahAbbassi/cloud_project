f361bfdb8d260f5e9f11f439639f39da2be927e7
altaf <altafallahabbassi@gmail.com>
2023-12-31 01:09:50 -0500

    constant file template for the proxy deisgn pattern

---
+++ gatekeeper/constants_template.py
@@ -0,0 +1,16 @@
+security_group_name = ''
+key_pair_name = ''
+ImageId =  ''
+subnet_id = ""
+arn = ''
+
+port=5000  # the port on which flask is running
+cluster_details = ''
+gatekeper_trusted_details = ''
+
+private_key_path =  ''
+gatekeeper_host_path = ''
+trusted_host_path = ''
+
+gatekeeper_source_code = ''
+trusted_host_source_code = ''
