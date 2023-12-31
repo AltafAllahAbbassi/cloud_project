042bec84d1a3293272b016f2a48bebd2beb68b71
altaf <altafallahabbassi@gmail.com>
2023-12-31 01:06:45 -0500

    required constants template file for proxy design pattern

---
+++ proxy/constants_template.py
@@ -0,0 +1,15 @@
+security_group_name = ''
+key_pair_name = ''
+ImageId =  ''
+subnet_id = ""
+arn = ''
+
+port= 5000 # flask running port 
+proxy_details = ''
+cluster_details = ''
+proxy_source_code = ''
+
+private_key_path =  ''
+proxy_host_path = ''
+
+
