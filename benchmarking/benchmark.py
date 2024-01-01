c1cfb34a13e633ce2e7f3461617fbef4df6359a1
altaf <altafallahabbassi@gmail.com>
2023-12-31 23:57:26 -0500

    script file to launch benchmark

---
+++ benchmarking/benchmark.py
@@ -0,0 +1,74 @@
+"""
+This file conatains the script to run the benchmark
+"""
+import boto3
+import paramiko
+import json
+from constants import private_key_path, cluster_instance_file, standalone_details
+
+# retriving the stanalone server and manager node (of sql cluser) dns 
+with open(cluster_instance_file) as file:
+    cluster_instance_details = json.load(file)
+with open(standalone_details) as file:
+    standalone_details = json.load(file)
+standalone_dns = standalone_details[0]['PublicDNS']
+for detail in cluster_instance_details:
+    if detail['Name'] =='manager':
+        cluster_manager_node_dns = detail['PublicDNS']
+        break
+
+
+# defining benchmarking commands on both standalone and manager node
+# we have defined three requests: read oly, write only and read_and_write
+# the result of benchmark is saved in files
+standalone_commands = [
+    'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1 --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_read_only.lua run > read_only_standalone.txt',
+    'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1 --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_write_only.lua run > write_only_standalone.txt ', 
+    'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1 --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_read_write.lua run > read_write_standalone.txt'
+]
+
+cluster_commands = [
+    f'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1  --mysql-host={cluster_manager_node_dns}  --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_read_only.lua run > read_only_cluster.txt',
+    f'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1  --mysql-host={cluster_manager_node_dns}  --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_write_only.lua run > write_only_cluster.txt',
+    f'sudo sysbench --db-driver=mysql --mysql-user=root    --mysql-db=sakila --range_size=100   --table_size=1000000 --tables=1 --threads=1  --mysql-host={cluster_manager_node_dns}  --events=0 --time=60   --rand-type=uniform /usr/share/sysbench/oltp_read_write.lua run > read_write_cluster.txt',
+
+]
+
+## seting ssh and running benchmarking commands
+ssh_standalone = paramiko.SSHClient()
+ssh_standalone.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_standalone.connect(standalone_dns, username='ubuntu', key_filename=private_key_path)
+
+
+ssh_manager = paramiko.SSHClient()
+ssh_manager.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_manager.connect(cluster_manager_node_dns, username='ubuntu', key_filename=private_key_path)
+
+
+for command in standalone_commands:
+    print("Running on standalone", command)
+    _,stdout, stderr = ssh_standalone.exec_command(command)
+    print(stdout.read().decode('utf-8'))
+    print(stderr.read().decode('utf-8'))
+
+    
+for command in cluster_commands: 
+    print("Running on standalone", command)
+    _,stdout, stderr = ssh_manager.exec_command(command)  
+    print(stdout.read().decode('utf-8'))
+    print(stderr.read().decode('utf-8'))
+
+
+# setting sftp to copy benchmark result files locally
+sftp_standalone = ssh_standalone.open_sftp()
+sftp_standalone.get("read_only_standalone.txt", "read_only_standalone.txt")
+sftp_standalone.get("write_only_standalone.txt", "write_only_standalone.txt")
+sftp_standalone.get("read_only_standalone.txt", "read_write_standalone.txt")
+sftp_standalone.close()
+
+
+sftp_cluster = ssh_standalone.open_sftp()
+sftp_cluster.get("read_only_cluster.txt", "read_only_cluster.txt")
+sftp_cluster.get("write_only_cluster.txt", "write_only_cluster.txt")
+sftp_cluster.get("read_write_cluster.txt", "read_write_cluster.txt")
+sftp_cluster.close()
